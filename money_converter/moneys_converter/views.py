124fbe046e00e10a28820c9a42b0d8421a997490
DemonMaike <dmitry.malyshevv@ya.ru>
2022-07-03 19:32:59 +0300

    append css and created index.htm

--- money_converter/moneys_converter/views.py
+++ money_converter/moneys_converter/views.py
@@ -1,12 +1,35 @@
+from urllib import response
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 
 def exchange(request):
-    name = 'mr. Anderson'
+    response_val = requests.get('https://api.exchangerate-api.com/v4/latest/USD').json()
+    currencies = response_val.get('rates')
     
-    context = {
-        'name': name
-    }
+    if request.metod == 'GET':
+        
+        context = {
+            'currencies':currencies,
+        }
+        
+        return render(request, 'moneys_converter/index.html', context)
     
-    return render(request, 'moneys_converter/index.html', context)
+    if request.method == "POST":
+        
+        form_amount = float(request.POST.get('form-amount'))
+        from_curr = request.POST.get('from-curr')
+        to_curr = request.POST.get('to-curr')
+        
+        converted_amount = round((currencies[to_curr] / currencies[from_curr]) * float(form_amount), 2)
+        
+        context = {
+            "form_amount": form_amount,
+            "from_curr": from_curr,
+            "to_curr": to_curr,
+            "converted_amount": converted_amount,
+        }
+        
+        return render(request, 'moneys_converter/index.html', context)
+    
